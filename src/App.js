9bfe9c7bd1cc3375c46bccf84e8f4b3b3b7ef876
Jacky Fan <mrf1013@outlook.com>
2022-02-04 15:38:54 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import ContactMe from './components/ContactMe';
 
 function App() {
   return (
     <main>
       <Header />
       <About />
+      <Projects />
+      <Skills />
+      <ContactMe />
     </main>
   );
 }
