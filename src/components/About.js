aa9e19b2348d93a14775ae8041d7d324e98ae0b2
Jacky Fan <mrf1013@outlook.com>
2022-02-04 16:02:01 +0800

    add personal info

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import dev from "./dev.png";
+import ReactRoundedImage from "react-rounded-image";
+import myIcon from "../img/icon.jpeg";
 
 export default function About() {
     return (
         <Container id="about">
-            <h1 style={{ width: "100", textAlign: "center" }}>About me</h1>
+            {/* <h1 style={{ width: "100", textAlign: "center" }}>About me</h1> */}
+            <p className="fs-1 text-center">About Me</p>
             <Row>
-                <Col sm={6} md={5} lg={{ span: 4, offset: 1 }}>
+                <Col sm={12} md={5} lg={6}>
                     <img
                         src={dev}
                         alt="Developer"
+                        width="100%"
                     ></img>
                 </Col>
-                <Col sm={6}>
-
+                <Col sm={12} lg={6}>
+                    <center>
+                        <ReactRoundedImage image={myIcon} />
+                    </center>
+                    <p className="fs-3 text-center font-monospace">Jacky FAN</p>
+                    <p className="text-center font-monospace lh-sm">
+                        An university student studing Computing in The Hong Kong Polytechnic University.
+                    </p>
                 </Col>
             </Row>
         </Container>
